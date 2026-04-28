11d6dd8ecd5a643a80088a32651db89b475190fb
ndovooformation <formation@ndovoo.com>
2026-04-28 16:38:17 +0300

    Initialisatio du composant Banner

---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+/**
+ * Banner est le composant pour présenter la bannière( la barre de navigation)
+ * Le composant Banner retourne un message codé en HTML <h1>Bienvenue à la maison</h1>
+ */
+
+const Banner = () => {
+    return <h1>Bienvenue à la maison</h1>
+}
+
+/**
+ * J'exporte le composant Banner e utilisant les mots-clés export default suivi du nom du composant
+ */
+export default Banner
