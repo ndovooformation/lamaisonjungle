bf52974ed207208b163c411252014ddd95a1787b
ndovooformation <formation@ndovoo.com>
2026-05-13 12:19:38 +0300

    Le panier est dynamique graĉe au useState

--- la-maison-jungle/src/components/Cart.jsx
+++ la-maison-jungle/src/components/Cart.jsx
@@ -5,21 +5,45 @@
 
 import styles from '../styles/Cart.module.css'
 
+import { useState } from 'react'
+
 const Cart = () => {
-    const montsera = 8
+    const monsteraPrice = 8
     const lierre = 12
     const bouquet = 9
+    const [cart, updateCart] = useState(0)
+    const [open, setOpen] = useState(false)
 
-    return (
+    return open ? (
         <div className={styles.cart}>
+            <button 
+                className={styles.lmjCartToogleButton} 
+                onClick={() => setOpen(false)}>
+                    Fermer
+            </button>
             <h2>Votre panier</h2>
-            <ul>
-                <li>Montsera: {montsera} €</li>
-                <li>Lierre: {lierre} €</li>
-                <li>Bouquet de fleurs: {bouquet} €</li>
-            </ul>
-            <p>Total : {montsera +  lierre + bouquet} €</p>
+        
+            <div>
+                Montsera: {monsteraPrice} €
+                <button 
+                    className={styles.lmjCartAddButton}
+                    onClick={() => updateCart(cart + 1)}>
+                        Ajouter
+                </button>
+            </div>
+          
+            <p>Total : {monsteraPrice * cart} €</p>
+            <button onClick={() => updateCart(0)}>Vider</button>
 
+        </div>
+        
+    ) : (
+        <div className={styles.lmjCartClosed}>
+            <button 
+                className={styles.lmjCartToogleButton} 
+                onClick={() => setOpen(true)}>
+                    Ouvrir le panier
+            </button>
         </div>
         
     )
